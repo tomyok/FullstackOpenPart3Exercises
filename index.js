ce9e6a039fbcc6a41b6fa7b49fa85bc71fef1ed3
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-04-18 20:24:33 -0300

    Ejercicio 3.2 finalizado

--- index.js
+++ index.js
@@ -34,5 +34,13 @@ app.get('/api/persons',(request, response) => {
     response.json(persons)
 })
 
+app.get('/info',(request, response) => {
+    let lengthPersons = persons.length
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${lengthPersons} people</p><p>${date}</p>`)
+})
+
 const PORT = 3001
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
