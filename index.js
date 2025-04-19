4eab26822c75e4ed9db3b1a73936ab100ce6ec67
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-04-18 23:24:12 -0300

    Ejercicio 3.3 finalizado

--- index.js
+++ index.js
@@ -6,38 +6,39 @@ let persons =
       {
         "name": "Arto Hellas",
         "number": "040-654321",
-        "id": "1"
+        "id": 1
       },
       {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
-        "id": "2"
+        "id": 2
       },
       {
         "name": "Dan Abramov",
         "number": "12-43-234345",
-        "id": "3"
+        "id": 3
       },
       {
         "name": "Mary Poppendieck",
         "number": "39-23-6423122",
-        "id": "4"
-      },
-      {
-        "id": "a4db",
-        "name": "asdasd",
-        "number": "231313"
+        "id": 4
       }
     ]
 
-app.get('/api/persons',(request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/info',(request, response) => {
+app.get('/info', (request, response) => {
     let lengthPersons = persons.length
     const date = new Date()
     response.send(`<p>Phonebook has info for ${lengthPersons} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    response.json(person)
 })
 
 const PORT = 3001
