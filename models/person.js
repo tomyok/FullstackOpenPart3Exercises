79e43f58a06616defe2abce07d2bcf56cf290cf7
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-04-26 12:42:22 -0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{8,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Person phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
