635b49c3d89c80c3ea0b2085d968986f40757464
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-04-26 13:00:53 -0300

    fix number person add

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function(v) {
-        return /^\d{2,3}-\d{6,}$/.test(v);
+        return /^\d{10,}$/.test(v);
       },
       message: props => `${props.value} is not a valid phone number!`
     },
